2e4229f5ebb1bb9806eeaefba0e06e23a9515946
Jesse205 <2055675594@qq.com>
2023-07-10 13:45:55 +0800

    fix: 部分图标显示异常

--- app/src/main/java/org/ohosdev/hapviewerandroid/util/BitmapUtil.java
+++ app/src/main/java/org/ohosdev/hapviewerandroid/util/BitmapUtil.java
@@ -5,16 +5,15 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.ColorMatrix;
 import android.graphics.ColorMatrixColorFilter;
-import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.renderscript.Allocation;
 import android.renderscript.Element;
 import android.renderscript.RenderScript;
 import android.renderscript.ScriptIntrinsicBlur;
 
-import org.ohosdev.hapviewerandroid.R;
-
 public class BitmapUtil {
+    private static final String TAG = "BitmapUtil";
 
     /**
      * <a href="https://www.jianshu.com/p/dc6120570cea">https://www.jianshu.com/p/dc6120570cea</a>
@@ -52,15 +51,20 @@ public class BitmapUtil {
     /**
      * 创建阴影 Bitmap
      *
+     * @param context 上下文对象
      * @param src 原始 Bitmap
+     * @param height 新图片高度
+     * @param width 新图片宽度
+     * @param padding 新图片边距
      * @return 阴影Bitmap
      */
-    public static Bitmap newShadowBitmap(Context context, Bitmap src,int padding,int width,int height) {
+    public static Bitmap newShadowBitmap(Context context, Bitmap src, int padding, int width, int height) {
         int srcWidth = src.getWidth();
         int srcHeight = src.getHeight();
 
         int newWidth;
         int newHeight;
+        // 计算新的宽高，使图片四周留出4dp边距
         if (srcWidth > srcHeight) {
             newWidth = width - padding * 2;
             newHeight = (int) ((float) newWidth / srcWidth * srcHeight + 0.5f);
@@ -69,21 +73,19 @@ public class BitmapUtil {
             newWidth = (int) ((float) newHeight / srcHeight * srcWidth + 0.5f);
         }
 
-        Matrix matrix = new Matrix();
-        matrix.postScale((float) newWidth / srcWidth, (float) newHeight / srcHeight);
-        Bitmap scaledSrcBitmap = Bitmap.createBitmap(src, 0, 0, srcWidth, srcHeight, matrix, false);
-
         Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
         Canvas canvas = new Canvas(bitmap);
         Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-
+        // 添加黑色滤镜，使图片变为黑色
         ColorMatrix colorMatrix = new ColorMatrix();
         colorMatrix.setScale(0, 0, 0, 0.3f);
         paint.setColorFilter(new ColorMatrixColorFilter(colorMatrix));
 
-        canvas.drawBitmap(scaledSrcBitmap, (float) (width - newWidth) / 2, (float) (height - newHeight) / 2, paint);
-
-        blurBitmap(context,bitmap);
+        Rect rect = new Rect(0, 0, newWidth, newHeight);
+        rect.offset((int) ((float) (width - newWidth) / 2), (int) ((float) (height - newHeight) / 2));
+        canvas.drawBitmap(src, null, rect, paint);
+        //模糊一下图片，使图片变虚，看起来好像阴影
+        blurBitmap(context, bitmap);
         return bitmap;
     }
 }
