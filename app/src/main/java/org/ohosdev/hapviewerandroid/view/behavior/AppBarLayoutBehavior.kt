1144a7eeaafb339b3645361be06034a1754bb877
Jesse205 <2055675594@qq.com>
2023-10-26 22:16:22 +0800

    refactor: 使用kt重写behavior

---
+++ app/src/main/java/org/ohosdev/hapviewerandroid/view/behavior/AppBarLayoutBehavior.kt
@@ -0,0 +1,40 @@
+package org.ohosdev.hapviewerandroid.view.behavior
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import com.google.android.material.appbar.AppBarLayout
+
+
+class AppBarLayoutBehavior : AppBarLayout.Behavior {
+    constructor() : super()
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+
+    override fun onNestedScroll(
+        coordinatorLayout: CoordinatorLayout,
+        child: AppBarLayout,
+        target: View,
+        dxConsumed: Int,
+        dyConsumed: Int,
+        dxUnconsumed: Int,
+        dyUnconsumed: Int,
+        type: Int,
+        consumed: IntArray
+    ) {
+        super.onNestedScroll(
+            coordinatorLayout,
+            child,
+            target,
+            dxConsumed,
+            dyConsumed,
+            dxUnconsumed,
+            dyUnconsumed,
+            type,
+            consumed
+        )
+        if (child.isLiftOnScroll && dyUnconsumed < 0) {
+            child.isLifted
+        }
+    }
+}
