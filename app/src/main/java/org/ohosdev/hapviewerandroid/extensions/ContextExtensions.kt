18ae747f7d55d7b652717a1b2d00cab99b2a26e6
Jesse205 <2055675594@qq.com>
2023-11-06 02:23:58 +0800

    refactor: 重构检查权限为ContextCompat

--- app/src/main/java/org/ohosdev/hapviewerandroid/extensions/ContextExtensions.kt
+++ app/src/main/java/org/ohosdev/hapviewerandroid/extensions/ContextExtensions.kt
@@ -2,9 +2,10 @@ package org.ohosdev.hapviewerandroid.extensions
 
 import android.content.Context
 import android.content.pm.PackageManager
+import androidx.core.content.ContextCompat
 import org.ohosdev.hapviewerandroid.app.HapViewerApp
 
 val Context.thisApp get() = applicationContext as HapViewerApp
 
 fun Context.isPermissionGranted(permission: String) =
-    checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED
+    ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED
