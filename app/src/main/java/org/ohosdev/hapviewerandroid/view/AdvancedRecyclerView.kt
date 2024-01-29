8751573efaa91466ae1892dbc6475283c9901383
Jesse205 <2055675594@qq.com>
2024-01-30 06:37:53 +0800

    fix: 修复大屏横向信息间距过大

---
+++ app/src/main/java/org/ohosdev/hapviewerandroid/view/AdvancedRecyclerView.kt
@@ -0,0 +1,40 @@
+package org.ohosdev.hapviewerandroid.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.ContextMenu.ContextMenuInfo
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class AdvancedRecyclerView : RecyclerView {
+    private var contextMenuInfo: RecyclerViewContextMenuInfo<ViewHolder>? = null
+
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    )
+
+    override fun getContextMenuInfo(): ContextMenuInfo? {
+        return contextMenuInfo
+    }
+
+    override fun showContextMenuForChild(originalView: View?): Boolean {
+        if (originalView == null || adapter == null) return false
+
+        val longPressPosition = getChildAdapterPosition(originalView)
+        if (longPressPosition >= 0) {
+            contextMenuInfo =
+                RecyclerViewContextMenuInfo(longPressPosition, getChildViewHolder(originalView))
+            return super.showContextMenuForChild(originalView)
+        }
+        return false
+    }
+
+    class RecyclerViewContextMenuInfo<T : ViewHolder>(
+        val position: Int,
+        val viewHolder: T
+    ) : ContextMenuInfo
+}
